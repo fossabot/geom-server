ff6f5816ae7d76dcd8cfd3304d755d9855fdea27
xezzon <xezzon@outlook.com>
2022-08-30 10:37:30 +0800

    添加 用户 实体类
    修改主键长度

---
+++ geom-db-system/src/main/java/indi/xezzon/geom/DbSystemApplication.java
@@ -0,0 +1,15 @@
+package indi.xezzon.geom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xezzon
+ */
+@SpringBootApplication
+public class DbSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DbSystemApplication.class, args);
+  }
+}
