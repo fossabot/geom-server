949526e8be40e7c0624e680534216559b3c33bec
xezzon <xezzon@outlook.com>
2022-09-15 16:26:34 +0800

    用户登录接口

--- geom-app-admin/src/main/java/indi/xezzon/geom/auth/adaptor/AuthController.java
+++ geom-app-admin/src/main/java/indi/xezzon/geom/auth/adaptor/AuthController.java
@@ -1,9 +1,11 @@
 package indi.xezzon.geom.auth.adaptor;
 
+import indi.xezzon.geom.auth.domain.LoginQuery;
 import indi.xezzon.geom.auth.domain.RegisterQuery;
 import indi.xezzon.geom.auth.domain.convert.UserConvert;
 import indi.xezzon.geom.auth.service.UserService;
 import indi.xezzon.geom.domain.User;
+import indi.xezzon.tao.logger.LogRecord;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +35,15 @@ public class AuthController {
   @PostMapping("/register")
   public User register(@RequestBody @Valid RegisterQuery user) {
     return userService.register(UserConvert.INSTANCE.from(user));
+  }
+
+  /**
+   * 用户登录
+   * @param query 用户名 密码
+   */
+  @PostMapping("/login")
+  @LogRecord
+  public void login(@RequestBody LoginQuery query) {
+    userService.login(query.getUsername(), query.getCipher());
   }
 }
