938d658f7b2405a71bdddff6a9136ddd00796455
xezzon <xezzon@outlook.com>
2022-09-30 15:49:11 +0800

    修复注册接口参数转换错误

--- geom-app-admin/src/main/java/indi/xezzon/geom/auth/domain/convert/UserConvert.java
+++ geom-app-admin/src/main/java/indi/xezzon/geom/auth/domain/convert/UserConvert.java
@@ -3,6 +3,7 @@ package indi.xezzon.geom.auth.domain.convert;
 import indi.xezzon.geom.auth.domain.query.RegisterQuery;
 import indi.xezzon.geom.auth.domain.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -13,5 +14,16 @@ public interface UserConvert {
 
   UserConvert INSTANCE = Mappers.getMapper(UserConvert.class);
 
+  /**
+   * 转换注册接口参数
+   * @param registerQuery 注册接口参数
+   * @return 用户模型
+   */
+  @Mapping(target = "updateTime", ignore = true)
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createTime", ignore = true)
+  @Mapping(target = "activateTime", ignore = true)
+  @Mapping(target = "cipher", ignore = true)
+  @Mapping(target = "plaintext", source = "cipher")
   User from(RegisterQuery registerQuery);
 }
