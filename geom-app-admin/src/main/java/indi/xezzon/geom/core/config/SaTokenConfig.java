dc6997dc9cb348d535ecef16bcd3ac2a6cef3d28
xezzon <xezzon@outlook.com>
2022-09-30 14:40:31 +0800

    继承 Redis 序列化会话

---
+++ geom-app-admin/src/main/java/indi/xezzon/geom/core/config/SaTokenConfig.java
@@ -0,0 +1,38 @@
+package indi.xezzon.geom.core.config;
+
+import static indi.xezzon.geom.core.constant.SpringConstants.BEAN_PREFIX;
+import static indi.xezzon.geom.core.constant.SpringConstants.SA_TOKEN_DAO;
+
+import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.dao.SaTokenDaoDefaultImpl;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xezzon
+ */
+@Configuration
+public class SaTokenConfig {
+}
+
+/**
+ * 动态选择会话序列化方式
+ * @deprecated 现在 sa-token 不支持动态选择，所以自己实现一下
+ */
+@Component
+@ConditionalOnProperty(
+    prefix = BEAN_PREFIX,
+    name = SA_TOKEN_DAO,
+    havingValue = "default",
+    matchIfMissing = true
+)
+@Deprecated
+class SaTokenDefaultDaoConfig implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) {
+    SaManager.setSaTokenDao(new SaTokenDaoDefaultImpl());
+  }
+}
--- geom-app-admin/src/main/java/indi/xezzon/geom/core/constant/SpringConstants.java
+++ geom-app-admin/src/main/java/indi/xezzon/geom/core/constant/SpringConstants.java
@@ -8,4 +8,6 @@ public class SpringConstants {
   public static final String BEAN_PREFIX = "bean";
 
   public static final String ID_GENERATOR = "id-generator";
+
+  public static final String SA_TOKEN_DAO = "sa-token-dao";
 }
