1edbcf7b04b0bc44079143d604aacbfb4cd26de8
xezzon <xezzon@outlook.com>
2023-01-03 15:14:49 +0800

    开放接口增删改查

---
+++ geom-app-admin/src/main/java/indi/xezzon/geom/open/dao/wrapper/WrappedPublicApiInstanceDAO.java
@@ -0,0 +1,22 @@
+package indi.xezzon.geom.open.dao.wrapper;
+
+import indi.xezzon.geom.external.jpa.JpaWrapper;
+import indi.xezzon.geom.open.dao.PublicApiInstanceDAO;
+import indi.xezzon.geom.open.domain.PublicApiInstance;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author xezzon
+ */
+public interface WrappedPublicApiInstanceDAO
+    extends JpaWrapper<PublicApiInstance, PublicApiInstanceDAO> {
+
+  /**
+   * 获取指定编码的实例
+   * @param code 接口编码
+   * @param ownerIds 所属用户
+   * @return 接口实例
+   */
+  List<PublicApiInstance> listByCode(String code, Collection<String> ownerIds);
+}
