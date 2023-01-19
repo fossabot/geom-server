6a254f3550b3f0905fc335db4965fb0e26044421
xezzon <yzjdx4399@gmail.com>
2023-01-19 16:31:59 +0800

    新增健康检查占位符

--- geom-app-admin/src/main/java/indi/xezzon/geom/AdminApplication.java
+++ geom-app-admin/src/main/java/indi/xezzon/geom/AdminApplication.java
@@ -3,6 +3,9 @@ package indi.xezzon.geom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xezzon
@@ -13,5 +16,15 @@ public class AdminApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AdminApplication.class, args);
+  }
+}
+
+@RequestMapping("/actuator")
+@RestController
+class ActuatorController {
+
+  @GetMapping("/health")
+  public String health() {
+    return "health";
   }
 }
